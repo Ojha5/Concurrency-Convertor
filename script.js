c750692266e64ca3698dcf36ce7f3ac4eef1eafc
om <omjha003@gmail.com>
2024-10-09 11:20:58 +0530

    Done with the logic of currency convertor and added the corresponding API

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
-
+let BASE_URL = "https://2024-03-06.currency-api.pages.dev/v1/currencies"; // Api end point
+let btn = document.querySelector(".btn")
 let selects = document.querySelectorAll(".dropdown select");
+let message = document.querySelector(".msg")
 
 for(let select of selects){
     for(let currCode in countryList){
@@ -8,11 +10,11 @@ for(let select of selects){
         newOption.innerText = currCode;
         select.append(newOption)
 
-        if(select.className == "from" && currCode == "INR"){
+        if(select.className == "from" && currCode == "USD"){
             newOption.selected = "selected";
         }
 
-        if(select.className == "to" && currCode == "USD"){
+        if(select.className == "to" && currCode == "INR"){
             newOption.selected = "selected"
         }
     }
@@ -28,3 +30,20 @@ let changeImage = (event) => {
     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
     event.previousElementSibling.src = newSrc
 }
+
+btn.addEventListener("click" , async() => {
+    let amount = document.querySelector(".amount input");
+    let amountVal = amount.value;
+    if(isNaN(amountVal) == true || amountVal <= 0){
+        amountVal = 1;
+        amount.value = "1";
+    }
+    
+    let fromCurr = selects[0].value;
+    let toCurr = selects[1].value;
+    let url = `${BASE_URL}/${fromCurr.toLowerCase()}.json`;
+    let response = await fetch(url);
+    let data = await response.json()
+    let rate = amountVal * data[fromCurr.toLowerCase()][toCurr.toLowerCase()];
+    message.innerText = `${amountVal} ${fromCurr} = ${rate} ${toCurr}`
+})
